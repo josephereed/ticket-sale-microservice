255da35b9b2ea1bd4c3b6c4553e0afcb8c4ca179
Joseph Reed <jereed559@gmail.com>
2020-08-17 00:15:17 -0700

    Initial commit

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,19 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+  public errors: ValidationError[];
+  constructor(errors: ValidationError[]) {
+    super('Invalid request parameters');
+    this.errors = errors;
+    // Only because extending a built-in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(err => {
+      return { message: err.msg, field: err.param };
+    });
+  }
+}
